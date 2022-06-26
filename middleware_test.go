a806d23ba545eee00e9cc0db32a820b0a93d3801
Adam Siegel <tobioatom@gmail.com>
2022-06-26 14:53:13 -0700

    adding middleware test

---
+++ middleware_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Role") != "user" {
+		w.Write([]byte("Not Authorized."))
+		return
+	}
+	w.Write([]byte("Welcome, User."))
+}
+
+func TestIsAuthorized(t *testing.T) {
+	IsAuthorized(testHandler)
+}
