df35e13b0e26c802536f3d64ae235ce4a08bef18
Adam Siegel <tobioatom@gmail.com>
2022-07-26 14:57:09 -0700

    debouncing

--- gpio.go
+++ gpio.go
@@ -13,7 +13,16 @@ func Initials(abc string) {
 	fmt.Println(abc)
 }
 
+var leftState = 0
+var rightState = 0
+
 func Left(d bool) {
+	if d == true && leftState == 2 {
+		return
+	}
+	if d == false && leftState == 1 {
+		return
+	}
 	offset1 := rpi.J8p31
 	v1 := 0
 	l1, err := gpiod.RequestLine("gpiochip0", offset1, gpiod.AsOutput(v1))
@@ -30,9 +39,20 @@ func Left(d bool) {
 	}
 	l1.SetValue(out)
 	time.Sleep(time.Millisecond * 17)
+	if d == true {
+		leftState = 2
+	} else {
+		leftState = 1
+	}
 }
 
 func Right(d bool) {
+	if d == true && rightState == 2 {
+		return
+	}
+	if d == false && rightState == 1 {
+		return
+	}
 	offset1 := rpi.J8p37
 	v1 := 0
 	l1, err := gpiod.RequestLine("gpiochip0", offset1, gpiod.WithPullUp, gpiod.AsOutput(v1))
@@ -49,6 +69,11 @@ func Right(d bool) {
 	}
 	l1.SetValue(out)
 	time.Sleep(time.Millisecond * 17)
+	if d == true {
+		rightState = 2
+	} else {
+		rightState = 1
+	}
 }
 
 func Start() {
